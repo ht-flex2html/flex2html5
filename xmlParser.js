3f146cbf4fdcc2145b1e996d3f7082f8badaf3a1
RayJohnMu <muruiqiang88@163.com>
2016-06-24 11:33:50 +0800

    test

--- xmlParser.js
+++ xmlParser.js
@@ -2,112 +2,137 @@ var Sax = require('sax');
 var Html = require('./htmlObj');
 var HtmlKind = require('./htmlKind');
 var StringBuffer=require('./stringBuffer');
+var HtmlStyleMap = require('./htmlStyleMap');
+var HtmlAttrMap = require('./htmlAttrMap');
 
 var HTMLElems = [];
 var HTMLElem,currentDom,rootTag,curTagName,arrtValue;
-var styleArray,extendArray,paramTagsArray,attributeParam;
-var scriptBuffer=new StringBuffer();
-var isTagClosed;
-var scriptDir="js_output/";
+var attributeParam = {};
+var styleArray,extendArray,paramTagsArray;
+var scriptBuffer = new StringBuffer();
+var tagTypes={"selfclose":0, "hassubtag":1, "hastext":2};
+var LastTagType;
+var scriptDir = "js_output/";
 var reg = /.*:/;
+var defindLog = "defind_";
 
 var XMLParser = (function () {
     function XMLParser(content) {
-      content=content.replace(/\r\n/g,"").replace(/\n/g,"");
-      this.parser = new Sax.parser(false);
-      
-      this.parser.onopentag = function (tag) {
-        curTagName=tag.name.replace(reg,"");
-        if(!rootTag)rootTag=tag;
+        content=content.replace(/\r\n/g,"").replace(/\n/g,"");
+        this.parser = new Sax.parser(false);
         
-        if (HTMLElem && attributeParam && !isTagClosed) {
-          HTMLElem.addAttributes(attributeParam);
-          currentDom = HTMLElem.addHtmlElementToDOM(currentDom);
-        }
-        if (HtmlKind.domTags.hasOwnProperty(curTagName)) {
-            HTMLElem=new Html(HtmlKind.domTags[curTagName]);
-            styleArray = new Array();
-            extendArray = new Array();
-            paramTagsArray = new Array();
+        this.parser.onopentag = function (tag) {
+            curTagName = tag.name.replace(reg, "");
+            console.log(curTagName);
+            if(!rootTag)rootTag=tag;
+            console.log(LastTagType);
+            if (HTMLElem && attributeParam && LastTagType > 0) {
+                HTMLElem.addAttributes(attributeParam);
+                currentDom = HTMLElem.addHtmlElementToDOM(currentDom);
+                if(LastTagType==tagTypes.hastext){
+                    currentDom = currentDom.parent();
+                }
+                HTMLElem = null;
+            }
             
-            for(var key in tag.attributes){
-              arrtValue = tag.attributes[key];
-              //将属性加入styleArray等
-              if(arrtValue){
+            if (HtmlKind.domTags.hasOwnProperty(curTagName)) {
+                HTMLElem=new Html(HtmlKind.domTags[curTagName]);
+                attrArray = new Array();
+                styleArray = new Array();
+                extendArray = new Array();
                 
-                  if(key == "HORIZONTALCENTER")
-                      arrtValue = HTMLElem.parseHorizontal(currentDom, arrtValue);
-                  if(key == "VERTICALCENTER")
-                      arrtValue = HTMLElem.parseVertical(currentDom, arrtValue);
+                for (var key in tag.attributes) {
+                    arrtValue = tag.attributes[key];
+                    //将属性加入styleArray等
+                    if (arrtValue) {
+                        if (HtmlKind.styleTags.hasOwnProperty(key)) { 
+                            var styleString = HtmlKind.styleTags[key] + ":" + arrtValue + "px";
+                            if (HtmlStyleMap.hasOwnProperty(key)) {
+                                styleString = HtmlStyleMap[key](HtmlKind.styleTags[key], currentDom, tag.attributes, arrtValue);
+                            }
+                            styleArray.push(styleString);
+                        } else if (HtmlKind.attrTags.hasOwnProperty(key)) {
+                            //根据标签，提取text属性
+                            if (HtmlAttrMap[curTagName]){
+                                if (HtmlAttrMap[curTagName](key, arrtValue)) {
+                                    attributeParam.text = arrtValue;
+                                }
+                            } else {
+                                attrArray.push(HtmlKind.attrTags[key] + "=" + arrtValue);
+                            }
+                        } else if (HtmlKind.extendTags.hasOwnProperty(key)) {
+                            extendArray.push(HtmlKind.extendTags[key] + "=" + arrtValue);
+                        } else {
+                            extendArray.push(defindLog + key + "=" + arrtValue);
+                        }
+                    }
+                }
 
-                  if (HtmlKind.styleTags.hasOwnProperty(key)) { 
-                    styleArray.push(HtmlKind.styleTags[key] + ":" + arrtValue + "px");
-                  } else if (HtmlKind.paramTags.hasOwnProperty(key)) {
-                      paramTagsArray.push(HtmlKind.paramTags[key] + "=" + arrtValue);
-                  } else if (HtmlKind.extendTags.hasOwnProperty(key)) {
-                      extendArray.push(HtmlKind.extendTags[key] + "=" + arrtValue);
-                  }
-              }
-          }
-            
-          attributeParam = {};
-          attributeParam.attributes =  paramTagsArray.join(" ");
-          attributeParam.style =  styleArray.join(";");
-          attributeParam.extend =  extendArray.join(" ");
-            
-          if(tag.isSelfClosing){
-            attributeParam.text="";
-            HTMLElem.addAttributes(attributeParam);
-            currentDom = HTMLElem.addHtmlElementToDOM(currentDom);
-            currentDom = currentDom.parent();   
-          }
-        } else {
-          HTMLElem = null;
+                attributeParam.attributes =  attrArray.join(" ");
+                attributeParam.style =  styleArray.join(";");
+                attributeParam.extend =  extendArray.join(" ");
+
+                LastTagType = tagTypes.hassubtag;
+                if (tag.isSelfClosing) {
+                    LastTagType = tagTypes.selfclose;
+                    HTMLElem.addAttributes(attributeParam);
+                    currentDom = HTMLElem.addHtmlElementToDOM(currentDom);
+                    currentDom = currentDom.parent();
+                    HTMLElem = null;
+                    attributeParam = {};
+                }
+            }
         }
-      }
-      this.parser.onattribute = function (attr) {
-        //可以对特定的属性做转换处理
-        //attr.name
-        //attr.value
-        //horizontalCenter-verticalCenter
-      };
-      this.parser.ontext = function (text) {
-        //标签文本内容处理
-        if(!text) attributeParam.text = text;
-      }
-      this.parser.oncdata =function (script) {
-        //处理Script block
-         scriptBuffer.append(script);
-         scriptBuffer.append("\r\n");
-      }
-      this.parser.onclosetag = function (tagName) {
-        tagName = tagName.replace(reg,"");
-        if(HtmlKind.domTags.hasOwnProperty(tagName) && tagName == curTagName){
-          isTagClosed = true;
+        this.parser.onattribute = function (attr) {
+          //可以对特定的属性做转换处理
+          //attr.name
+          //attr.value
+          //horizontalCenter-verticalCenter
+        };
+        this.parser.ontext = function (text) {
+          //标签文本内容处理
+            if(text.replace(/\t/g,"").length >0){
+                attributeParam.text = text;
+            }       
         }
-        if(tagName == rootTag.name.replace(reg,"") && scriptBuffer.toString().length >0){
-          //TODO:将script 生成到文件
-          var class_name = "test";
-           var fs = require("fs");
-           var path = require('path');
-           fs.createFileSync(path.resolve(scriptDir+class_name+".as"), scriptBuffer.toString());
+        this.parser.oncdata =function (script) {
+          //处理Script block
+          scriptBuffer.append(script);
+          scriptBuffer.append("\r\n");
         }
-      }
-      
-      this.parser.onend = function () {
-        //全部解析完毕后处理
-      }
-      this.parser.onerror = function (e) {
-        //当解析出错时处理
-      };
-      this.parser.write(content).end();
-      
-      // Sax.EVENTS.forEach(function (ev) {
-      //   var eve=ev;
-      // });
+        this.parser.onclosetag = function (tagName) {
+            tagName = tagName.replace(reg,"");
+            //父标签关闭时设置
+            if(HtmlKind.domTags.hasOwnProperty(tagName)){
+                if(tagName == curTagName){
+                    LastTagType = tagTypes.hastext;
+                } else {
+                    currentDom = currentDom.parent();
+                }
+            }
+            if (tagName == rootTag.name.replace(reg,"") && scriptBuffer.toString().length >0) {
+              //TODO:将script 生成到文件
+                var class_name = "test";
+                var fs = require("fs");
+                var path = require('path');
+                var filePath = scriptDir + class_name + ".as";
+                fs.createFileSync(path.resolve(filePath), scriptBuffer.toString());
+                Html.addScriptLink(filePath);
+            }
+        }
+        
+        this.parser.onend = function () {
+          //全部解析完毕后处理
+        }
+        this.parser.onerror = function (e) {
+          //当解析出错时处理
+        }
+        this.parser.write(content).end();
+
     }
+
     XMLParser.prototype.outStream= function () {
-        return HTMLElem.outStream();
+        return Html.outStream();
     }
     return XMLParser;
 })();
