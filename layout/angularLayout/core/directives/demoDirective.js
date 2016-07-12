f8f0bcd903f71a0eb24c5d66ba1db83bc30071b1
RayJohnMu <muruiqiang88@163.com>
2016-07-12 11:23:02 +0800

    rwar

---
+++ layout/angularLayout/core/directives/demoDirective.js
@@ -0,0 +1,11 @@
+(function() {
+    angular
+        .module("myApp")
+        .directive('demoDirective', ['$scope', demoDirective]);
+        
+    function demoDirective() {
+        return {
+            restrict: 'AE'
+        };
+    }
+})()
